f841efd36d1143cf56ad055b03eb2bb9efe032bb
grishavasilyan <grisha.vasilyan@volo.global>
2025-03-29 13:42:50 +0400

    product details page functionality

--- src/routing/Routing.jsx
+++ src/routing/Routing.jsx
@@ -5,7 +5,7 @@ import { Home } from '../pages/Home';
 import { Images } from '../pages/Images';
 import { ImageDetail } from '../pages/ImageDetail';
 import { Subscriptions } from '../pages/Subscriptions';
-import { Products } from '../pages/Products';
+import { Products } from '../pages/products/Products';
 
 // Route groups
 import { PrivateRoutes } from './PrivateRoutes';
@@ -28,7 +28,7 @@ export const Routing = () => {
                         <Route path='/images' element={<Images />} />
                         <Route path='/images/:imageId' element={<ImageDetail />} />
                         <Route path='/subscriptions' element={<Subscriptions />} />
-                        <Route path='/products' element={<Products />} />
+                        <Route path='/products?/:productId' element={<Products />} />
                     </Route>
                 </Route>
             </Routes>
