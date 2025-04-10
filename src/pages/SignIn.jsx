e925616503950acb9b4a439174935d7fafea70ac
grishavasilyan <grisha.vasilyan@volo.global>
2025-04-10 17:50:23 +0400

    remove package from few other files

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-dom';
 
 export const SignIn = () => {
     const navigate = useNavigate();
